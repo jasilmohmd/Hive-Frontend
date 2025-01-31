ebcd6cafbad5e16ca84d6032556edf2e161e040a
jasilmohmd <jasilcheruppa@gmail.com>
2025-01-31 18:46:57 +0530

    merge conflict fix

--- src/app/services/user-auth.service.ts
+++ src/app/services/user-auth.service.ts
@@ -4,8 +4,6 @@ import { Injectable } from '@angular/core';
 import { ILoginCredentials, IRegisterationCredentials, IUser } from '../models/user';
 import { catchError, throwError, Observable } from 'rxjs';
 
-import { catchError, throwError, Observable } from 'rxjs';
-
 @Injectable({
   providedIn: 'root'
 })
